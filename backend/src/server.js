6528c86d407635ddfe0fe82e412dcbc56cbace86
Swastik Bose <swastikbose31@gmail.com>
2026-01-04 17:26:05 +0530

    utils added with middlewares

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,18 @@ dotenv.config({
 });
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!!", err);
+    })
+
+
+
+
 /*import express from "express";
 
 const app = express()
