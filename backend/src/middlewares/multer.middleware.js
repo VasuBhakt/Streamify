67373696b88328262a17a0482a2b59f24d89abb4
Swastik Bose <swastikbose31@gmail.com>
2026-01-05 16:57:27 +0530

    fixes 2

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -2,11 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({ // disk storage instead of memory storage
     destination: function (req, file, cb) {
-        cb(null, "../../public/temp")
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname) // fieldname is the name of the file in the form
+        cb(null, file.originalname) // fieldname is the name of the file in the form
     }
 })
 
--- backend/src/utils/Cloudinary.js
+++ backend/src/utils/Cloudinary.js
@@ -2,18 +2,19 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    apiKey: process.env.CLOUDINARY_API_KEY,
-    apiSecret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
-        const response = await cloudinary.v2.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         })
         console.log(`File uploaded successfully on Cloudinary! ${response.secure_url}`);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload failed
