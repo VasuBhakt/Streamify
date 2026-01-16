dda5628ddf61a8f9903a4929442da65f2f6bf1f4
Swastik Bose <swastikbose31@gmail.com>
2026-01-16 13:13:42 +0530

    likes service added

---
+++ frontend/src/services/like.js
@@ -0,0 +1,32 @@
+export class LikeService {
+
+    async toggleLikeVideo({ videoId }) {
+        try {
+            const response = await axios.post(`/likes/${videoId}`);
+            return response.data;
+        } catch (error) {
+            console.log("LikeService :: toggleLikeVideo :: error", error);
+            throw error;
+        }
+    }
+
+    async toggleLikeComment({ commentId }) {
+        try {
+            const response = await axios.post(`/likes/${commentId}`);
+            return response.data;
+        } catch (error) {
+            console.log("LikeService :: toggleLikeComment :: error", error);
+            throw error;
+        }
+    }
+
+    async userLikedVideos({ page = 1, limit = 10 }) {
+        try {
+            const response = await axios.get(`/likes/liked-videos`, { params: { page: page || 1, limit: limit || 10 } });
+            return response.data;
+        } catch (error) {
+            console.log("LikeService :: userLikedVideos :: error", error);
+            throw error;
+        }
+    }
+}
