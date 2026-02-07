6fe395a6ca26ce83989bd3d6645b081b113d12d0
Swastik Bose <swastikbose31@gmail.com>
2026-02-07 22:42:12 +0530

    multer mrewritten for vercel compatibility

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,12 +1,13 @@
 import multer from "multer";
+import os from "os";
 
 const storage = multer.diskStorage({ // disk storage instead of memory storage
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        cb(null, os.tmpdir());
     },
     filename: function (req, file, cb) {
-        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.originalname) // fieldname is the name of the file in the form
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + "-" + file.originalname);
     }
 })
 
